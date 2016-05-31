3f5aadefe0f5f7c17ce3c42b196b3656c761aa0a
Edgar <eardon012@outlook.com>
2016-05-31 07:03:40 -0600

    modified main script gives out error message when run

--- problemas1y2/problema2/src/problema2.sh
+++ problemas1y2/problema2/src/problema2.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-DATA=~/Documents/LinuxServerAdmin2/proyectoFinal/problema2
+DATA=~/Documents/LX2_Edgar/problemas1y2/problema2
 
-mkdir $DATA/archivos_csv
-OUT_DATA=$DATA/archivos_csv
+mkdir $DATA/datos_$(date +%y.%m.%d)_csv
+OUT_DATA=$DATA/datos_$(date +%y.%m.%d)
 
 m=0
 
-for i in `find $DATA -name '*.xls'`
+for i in `find $DATA\hojasDatos -name '*.xls'`
 do 
 	echo "Procesando archivo $i"
 
 	xls2csv $i > $OUT_DATA/data-$m.csv
 	let m=m+1
-	done 2> error1.log
+done 2> error1.log
