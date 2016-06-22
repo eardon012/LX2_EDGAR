16f19e74294ecd465311d0c7794ac14e223b2755
Edgar <eardon012@outlook.com>
2016-06-21 20:26:21 -0600

    problema3 completed

---
+++ problema3/src/problema3.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+clear 
+
+echo "################################################################"
+echo "#   This program turns XLS files into CSV files                #"
+echo "#   then it takes out all the commas so it can be plotted with #"
+echo "#   GNUplot  (Problema 3)                                      #"
+echo "################################################################"
+DATA=~/Documents/LX2_EDGAR/problema3
+
+mkdir $DATA/plot
+DATAPLOT=$DATA/plot
+echo " creating plot folder to move any CSV file(s) before plotting "
+mkdir $DATA/archivos_csv
+echo " creating archivos_csv folder to drop CSV files into "
+mkdir $DATA/png
+DATAPNG=$DATA/png
+echo " creating png folder to drop png files into "
+DATACSV=$DATA/archivos_csv
+
+m=0
+
+for i in `find $DATA/hojasDatos -name '*.xls'`
+do 
+	echo "Processing file $i"
+	xls2csv -q 0 $i > $DATACSV/data-$m.csv
+        echo " Getting CSV data-$m.csv ready for GNUplot "
+        ls -lh $DATACSV
+	awk -F"," '{print $1,$2,$3}' $DATACSV/data-$m.csv >> $DATAPLOT/plot$m.dat
+        echo "plot$m.dat File ready"
+        ls -lh $DATAPLOT
+
+/usr/bin/gnuplot <<__EOF 
+
+reset
+
+set terminal png
+
+set output"$DATAPNG/plot$m.png"
+
+set xlabel "CLIMA PRO RECORD" 
+set xrange [1:1812]
+
+set ylabel "kW m2"
+set yrange [0:3]
+
+set grid
+
+set title "Plot $m"
+
+set style data linespoints
+
+plot "$DATAPLOT/plot$m.dat" using 1:2 title "Max", "" using 1:3 title "Min"
+__EOF
+
+let m=m+1
+
+done 
+
+echo "All tasks completed"
+
+
